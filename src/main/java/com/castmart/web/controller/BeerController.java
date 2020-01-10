0971192c0d408cbb8dd3774397c1d8e26fad49ea
castmart <jc.castmart@gmail.com>
2020-01-09 19:22:51 -0600

    Beer repository and Beer model (JPA)

--- src/main/java/com/castmart/web/controller/BeerController.java
+++ src/main/java/com/castmart/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package com.castmart.web.controller;
 
 import com.castmart.web.model.BeerDTO;
+import com.castmart.web.model.BeerStyleEnum;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +16,16 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId) {
         // TODO impl
-        return new ResponseEntity(BeerDTO.builder().build(), HttpStatus.OK);
+        return new ResponseEntity(BeerDTO.builder().id(beerId).beerName("Coronita").beerStyle(BeerStyleEnum.LAGER).build(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDTO beerDTO) {
-        // TODO impl
-        return new ResponseEntity(HttpStatus.CREATED);
+        BeerDTO dto = BeerDTO.builder().id(UUID.randomUUID()).beerName(beerDTO.getBeerName()).beerStyle(BeerStyleEnum.LAGER).build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/"+dto.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
